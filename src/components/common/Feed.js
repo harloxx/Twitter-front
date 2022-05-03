d37e6c5c0f4917237abb91d0c63a1d9908000f20
flowersayo <flowersayo0811@ewhain.net>
2022-05-03 13:35:10 +0900

    <feat> 피드 컴포넌트

---
+++ src/components/common/Feed.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import TweetBox from "./TweetBox";
+import Post from "./Post";
+import "./Feed.css";
+
+import FlipMove from "react-flip-move";
+
+function Feed() {
+  const [posts, setPosts] = useState([
+    {
+      text: "text1",
+      displayName: "displayName",
+      username: "username",
+      verified: "verified",
+
+      avatar: "avatar",
+      image:
+        "https://pbs.twimg.com/media/FPZ2cTxUcAEd0g-?format=jpg&name=large",
+    },
+    {
+      text: "text2",
+      displayName: "displayName",
+      username: "username",
+      verified: "verified",
+
+      avatar: "avatar",
+      image:
+        "https://pbs.twimg.com/media/FPZ2cTxUcAEd0g-?format=jpg&name=large",
+    },
+  ]);
+
+  useEffect(() => {
+    /*db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => doc.data()))
+    );*/
+  }, []);
+
+  return (
+    <div className="feed">
+      <div className="feed__header">
+        <h2>Home</h2>
+      </div>
+
+      <TweetBox />
+
+      <FlipMove>
+        {posts.map((post) => (
+          <Post
+            key={post.text}
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))}
+      </FlipMove>
+    </div>
+  );
+}
+
+export default Feed;
